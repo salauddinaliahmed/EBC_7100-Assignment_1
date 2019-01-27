ad8c6118b07b091dec7a28305b0d56e5f929b380
Jiaxi68 <35809250+Jiaxi68@users.noreply.github.com>
2019-01-27 13:57:07 -0500

    DRAFT

--- test_1.py
+++ test_1.py
@@ -16,3 +16,41 @@ for each_item in temp_list:
     print (each_item)
     print ("\n")
     
+    
+    import nltk
+nltk.download('gutenberg')
+nltk.download('punkt')
+from nltk.corpus import gutenberg 
+import random
+import os
+import glob
+import sys
+import errno
+
+
+#files_en = gutenberg.fileids()      # Get file ids
+book_1 = gutenberg.open('whitman-leaves.txt').readlines()
+book_2 = gutenberg.open('melville-moby_dick.txt').read()
+book_3 = gutenberg.open('shakespeare-caesar.txt').read()
+
+sentence_detector = nltk.data.load('tokenizers/punkt/{0}.pickle'.format('english'))
+listOfSentences = []
+print (type(book_1))
+
+for text in book_1:
+    try:
+        listOfSentences += [x.replace("\n", " ").replace("  "," ").strip() for x in random.sample(sentence_detector.tokenize(text), 200)]
+
+    except IOError as exc:
+    # Do not fail if a directory is found, just ignore it.
+        if exc.errno != errno.EISDIR:
+            raise
+
+random_sample_input = random.sample(listOfSentences, 15)
+print(random_sample_input)
+
+# This block of code writes the result of the previous to a new file
+random_sample_output = open("randomsample", "w", encoding='utf-8')
+random_sample_input = map(lambda x: x+"\n", random_sample_input)
+random_sample_output.writelines(random_sample_input)
+random_sample_output.close()
