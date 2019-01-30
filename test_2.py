d0a151b2f7e01291ab092be49bb34a526db86ba6
Salauddin Ali Ahmed <salauddinaliahmed@gmail.com>
2019-01-30 16:54:27 -0500

    Code upto creating word records of 200 documents.

--- test_2.py
+++ test_2.py
@@ -1,6 +1,6 @@
 # Working code for collection chucks from each book. 
-# We now have each chunck containing 30 sentences from each book. This will give us enough chunks to sample 200 from each book.
-from nltk.corpus import gutenberg
+# We now have each chunck containing 30 sentences from each book. This will give us enough chunks to sample 200 from each
+from nltk.corpus import gutenberg, stopwords
 import random
 import nltk
 book_1 = gutenberg.raw('bible-kjv.txt')
@@ -17,7 +17,25 @@ for author, book in dict_labels.items():
     for i in range(limit):
         master_list.append((book[n:n+30], author))
         n+=31
+print (size_of_books)
+complete_book_1 = master_list[0:size_of_books[0]-1]
+complete_book_2 = master_list[961:1278]       
+complete_book_3 = master_list[1278:-1]
+total_sample = [complete_book_1, complete_book_2, complete_book_3]
+for i in total_sample:
+    print ("Total number of documents in each book are", len(i))
 
+#Random Sampling 200 documents from each book. 
 
-        
+sample_book1 = random.sample(complete_book_1, 200)
+sample_book2 = random.sample(complete_book_2, 200)
+sample_book3 = random.sample(complete_book_3, 200)
+
+#Preprocessing the data, by taking 150 top frequency words excluding the stop words from each document. 
+book1_words = [nltk.word_tokenize(" ".join(each_doc).lower()) for each_doc in sample_book1]
+book2_words = [nltk.word_tokenize(" ".join(each_doc).lower()) for each_doc in sample_book2]
+book3_words = [nltk.word_tokenize(" ".join(each_doc).lower()) for each_doc in sample_book3]
+
+#initializing stopwords
+stop_words = stopwords.words("english")
 
