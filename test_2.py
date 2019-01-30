c393ddae5d1c936739d421bc7857ce8d22d7e157
Salauddin Ali Ahmed <salauddinaliahmed@gmail.com>
2019-01-30 12:42:50 -0500

    Added a code to save a list of tuples containing author name and a list item of 30 sentences.

--- test_2.py
+++ test_2.py
@@ -1,17 +1,23 @@
+# Working code for collection chucks from each book. 
+# We now have each chunck containing 30 sentences from each book. This will give us enough chunks to sample 200 from each book.
 from nltk.corpus import gutenberg
 import random
 import nltk
 book_1 = gutenberg.raw('bible-kjv.txt')
-book_1 = nltk.sent_tokenize(book_1)
-user_list = []
-print (len(book_1))
-n = 0;
-limit = len(book_1)/30
-for i in range(int(limit)):
-    user_list.append(book_1[n:n+30])
-    n+=31
-print (len(user_list))
-new_sample = random.sample(user_list, 200)
-record_string = " ".join(new_sample[1])
-record_word_tokenized = nltk.word_tokenize(record_string)
-print (len(record_word_tokenized))
+book_2 = gutenberg.raw('melville-moby_dick.txt')
+book_3 = gutenberg.raw('edgeworth-parents.txt')
+dict_labels = {'KJV (Bible)': book_1, 'Herman Melville (Moby-Dick)' : book_2, 'Richard Lovell Edgeworth (Parents)' : book_3}
+master_list = []
+# Dividing each book into documents of 30 sentences. appending them to *master_list*. 
+for author, book in dict_labels.items():
+    n = 0;
+    book = nltk.sent_tokenize(book)
+    limit = int(len(book)/31)
+    print (author, " number of 30 chunks is:" , limit)
+    for i in range(limit):
+        master_list.append((book[n:n+30], author))
+        n+=31
+
+
+        
+
